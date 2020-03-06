844b1208669c9373e119227d517c78da1ed0765c
boxshow <boxshow@qq.com>
2020-03-06 19:52:28 +0800

    2020/3/6

---
+++ uni-app/mf-com/App.vue
@@ -0,0 +1,23 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+	/*官方样式库*/
+	@import url("./common/uni.css");
+	/*自定义图标库*/
+	@import url("./common/icon.css");
+	/*引用动画库*/
+	@import url("./common/animate.css");
+</style>
---
+++ uni-app/mf-com/pages/index/index.vue
@@ -0,0 +1,34 @@
+<template>
+	<view>
+
+		<!--view class="iconfont icon-daohanglan-03" style="margin-top:200rpx;display:flex;justify-content: center; font-size: 100rpx;color: red;">
+		</view-->
+		<view style="display:flex;justify-content: center;padding: 100rpx;">
+			<view class="animated " hover-class="bounceIn" style="font-size: 100rpx;color: red;">
+				{{contents}}
+			</view>
+		</view>
+
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello',
+				contents:'World',
+			}
+		},
+		onLoad() {
+			
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+
+</style>
